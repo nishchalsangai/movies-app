f0e4a836df9bce46567041fc7f9ce036dbff7a7c
Nishchal Sangai <41857915+nishchalsangai@users.noreply.github.com>
2023-12-20 01:10:32 +0530

    Search UI in progress

---
+++ lib/modules/movies/domain/entity/search_entity.dart
@@ -0,0 +1,5 @@
+class SearchEntity {
+  String query;
+  int page;
+  SearchEntity(this.query, this.page);
+}
---
+++ lib/modules/movies/presentation/widgets/search_widget.dart
@@ -0,0 +1,55 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movies_app/core/helpers/app_theme.dart';
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({super.key, required this.controller});
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        fillColor: AppTheme.chipBackground,
+        filled: true,
+        hintText: 'TV shows, movies and more',
+        hintStyle: const TextStyle(fontSize: 14, color: AppTheme.bodyTextColor),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(35)),
+        errorBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(35)),
+        disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(35)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(35)),
+        border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(35)),
+        prefixIcon: const Hero(
+          tag: 'search-hero',
+          child: Icon(Icons.search),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {
+            context.pop();
+          },
+          child: Transform.rotate(
+              angle: math.pi / 4,
+              child: const Icon(
+                Icons.add,
+                size: 30,
+              )),
+        ),
+      ),
+    );
+  }
+}
